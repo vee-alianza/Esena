52affe278fb3657a6e0f61811750c7047f8dd2fd
ethanchen7 <ethan.chen7@gmail.com>
2022-05-27 12:50:53 -0700

    create project form

---
+++ react-app/src/context/Modal.js
@@ -0,0 +1,36 @@
+import { createContext, useContext, useRef, useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import "./modal.css";
+
+export const ModalContext = createContext();
+
+const ModalProvider = ({ children }) => {
+  const modalRef = useRef();
+  const [value, setValue] = useState();
+
+  useEffect(() => {
+    setValue(modalRef.current);
+  }, []);
+
+  return (
+    <>
+      <ModalContext.Provider value={value}>{children}</ModalContext.Provider>
+      <div ref={modalRef} />
+    </>
+  );
+};
+
+export function Modal({ onClose, children }) {
+  const modalNode = useContext(ModalContext);
+  if (!modalNode) return null;
+
+  return ReactDOM.createPortal(
+    <div id="modal">
+      <div id="modal-background" onClick={onClose} />
+      <div id="modal-content">{children}</div>
+    </div>,
+    modalNode
+  );
+}
+
+export default ModalProvider;
