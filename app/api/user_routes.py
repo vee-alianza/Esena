29ee4a0f844f257641f1bdc90cf6a13adb5a4d3a
ethanchen7 <ethan.chen7@gmail.com>
2022-05-27 16:15:08 -0700

    user_routes changes, auth_routes changes, initial redux added

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -20,28 +20,50 @@ def validation_errors_to_error_messages(validation_errors):
 @user_routes.route('/')
 @login_required
 def users():
-    users = User.query.all()
-    return {'users': [user.to_dict() for user in users]}
 
+    # gets all users (for search)
+
+    users = User.query.all()
+    return [user.to_dict() for user in users]
+    
+    # for user in users:
+    #     user_dict = user.to_dict()
+
+        # owned_projects = {project.id: project.to_dict() for project in user.owned_projects}
+        # joined_projects = {project.id: project.to_dict() for project in user.joined_projects}
+
+        # assigned_tasks = {task.id: task.to_dict() for task in user.assigned_tasks}
+
+        # user_dict["owned_projects"] = owned_projects
+        # user_dict["joined_projects"] = joined_projects
+        # user_dict["assigned_tasks"] = assigned_tasks
+        
+    #     all_users[user.id] = user_dict
+
+    # return all_users
 
 @user_routes.route('/<int:id>')
 @login_required
 def user(id):
     user = User.query.get(id)
-    # print("******************", user.to_dict())
-    # return user.to_dict()
     user_dict = user.to_dict()
-
-    # print("************", user.owned_projects)
 
     owned_projects = {project.id: project.to_dict() for project in user.owned_projects}
     joined_projects = {project.id: project.to_dict() for project in user.joined_projects}
 
     assigned_tasks = {task.id: task.to_dict() for task in user.assigned_tasks}
 
+    projects = list(owned_projects.values()) + list(joined_projects.values())
+    teammates = []
+    for project in projects:
+        teammates.extend(project["members"]) 
+    teammates = list(set(teammates))
+    teammates.remove(id)
+
     user_dict["owned_projects"] = owned_projects
     user_dict["joined_projects"] = joined_projects
     user_dict["assigned_tasks"] = assigned_tasks
+    user_dict['teammates'] = teammates
 
     return user_dict
 
