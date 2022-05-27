73740274ea72cb14d53e0c77fcd4442cbd4c50d7
xwnnie <niexiaowen90@gmail.com>
2022-05-27 10:18:25 -0700

    PUT tasks complete

---
+++ app/api/task_routes.py
@@ -0,0 +1,45 @@
+from flask import Blueprint, jsonify, request, render_template
+from flask_login import login_required, current_user
+from app.models import db, User, Project, Task
+from app.forms.delete_form import DeleteForm
+from app.forms.task_form import TaskForm
+
+task_routes = Blueprint('tasks', __name__)
+
+def validation_errors_to_error_messages(validation_errors):
+    """
+    Simple function that turns the WTForms validation errors into a simple list
+    """
+    errorMessages = []
+    for field in validation_errors:
+        for error in validation_errors[field]:
+            errorMessages.append(f'{field} : {error}')
+    return errorMessages
+
+@task_routes.route('/<int:id>', methods=["PUT"])
+#commented out for test only
+# @login_required
+def update_task(id):
+    """
+    Updates a task
+    """
+    #check if current_user.id == assigner_id:
+    
+    form = TaskForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        task = Task.query.get(id)
+        if task:
+            task.name = form.data['name']
+            task.description = form.data['description']
+            task.end_date = form.data['end_date']
+            task.is_completed = form.data['is_completed']
+            task.priority_id = form.data['priority_id']
+            task.status_id = form.data['status_id']
+            task.assignee_id = form.data['assignee_id']
+            db.session.commit()
+            return task.to_dict()
+        else:
+            return {'errors': ['Task not found.']}, 404
+
+    return {'errors': validation_errors_to_error_messages(form.errors)}, 401
--- routes_test.py
+++ routes_test.py
@@ -3,7 +3,8 @@ from datetime import datetime
 
 # url = "http://127.0.0.1:5000/api/users/4/projects"
 # url = "http://127.0.0.1:5000/api/projects/5"
-url = "http://127.0.0.1:5000/api/projects/2/tasks"
+# url = "http://127.0.0.1:5000/api/projects/2/tasks"
+url = "http://127.0.0.1:5000/api/tasks/3"
 # payload={'name': 'esena project',
 #     'description': 'Sqlalchemy is a must',
 #     'start_date': '2022-3-1',
@@ -12,12 +13,12 @@ url = "http://127.0.0.1:5000/api/projects/2/tasks"
 #     'priority_id': 2,
 #     'status_id': 3,
 # }
-payload={'name': 'design',
+payload={'name': 'design doc',
     'description': 'Docs, wireframes',
-    'end_date': '2022-4-1',
+    'end_date': '2022-3-1',
     'priority_id': 2,
     'status_id': 3,
-    'assignee_id': 3
+    'assignee_id': 2
 }
 
 headers = {
@@ -26,6 +27,6 @@ headers = {
   'Cookie': 'session=.eJwljjtuAzEMRO-i2oXEnyRfZkGKJBIYSIBduzJ89whINzPFm_cuR55xfZX783zFrRzfXu4FhkonUzCGnYSX9EqdUAEb2gDzuZxjJUXowC5QNUOiLRAeQ6aRUu_hONMHA3iYTonwTES2ya4mNiRAiJvaXGnkVCuSZC1b5HXF-W_Tdl3Xmcfz9xE_ewjQOTfAjdCV942YIwo7LeaIRswTxyqfPzRLQH4.YpBajg.mp1qTPgMLlXaOju098J4MWJPOtQ; csrf_token=ImUyYTk5ZGFiZGI0M2RhNTdlZDZiZDMzNjVkNGM1NWVlMTQ1NTkzOGMi.YpEEjA.YjJuv8OpfdnhJrjB9mOcxY5nqB8'
 }
 
-response = requests.request("POST", url, headers=headers, data=payload)
+response = requests.request("PUT", url, headers=headers, data=payload)
 
 print(response.text)
