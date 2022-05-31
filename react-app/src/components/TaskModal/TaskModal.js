b3e9d118582e3b8d14302af1077389415235dc27
Lana_K <lana1990.komar@gmail.com>
2022-05-31 15:44:54 -0700

    styled single task modal

--- react-app/src/components/TaskModal/TaskModal.js
+++ react-app/src/components/TaskModal/TaskModal.js
@@ -35,19 +35,25 @@ const TaskModal = (taskId) => {
     }
 
     return (
-        <div className="task">
+        <div className="task-container">
             <div className="task-overview">
-                <div>{task?.name}</div>
-                <div>
+                <div className="task-name">{task?.name}</div>
+                <div className="about-task">
                     <div>Project:</div>
                     <div>{projects[task?.project_id]?.name}</div>
                     <div>Assignee:</div>
                     <div>{users[task?.assignee_id]?.first_name} {users[task?.assignee_id]?.last_name}</div>
                     <div>Due Date:</div>
                     <div>{task?.end_date}</div>
-                    <div>Priority</div>
+                    <div>
+                        <i className="fa-regular fa-circle-check fa-lg"></i>
+                        Priority
+                    </div>
                     <div>{task?.priority}</div>
-                    <div>Status:</div>
+                    <div>
+                        <i className="fa-regular fa-circle-check fa-lg"></i>
+                        Status:
+                    </div>
                     <div>{task?.status}</div>
                     <div>Description:</div>
                     <div>{task?.description}</div>
@@ -61,23 +67,24 @@ const TaskModal = (taskId) => {
                             <p>{getInitials(comment.author_id)}</p>
                         </div>
                         <div className="comment-content">
-                            <p>{users[comment?.author_id]?.first_name} {users[comment?.author_id]?.last_name}</p>
+                            <p className="author-name">{users[comment?.author_id]?.first_name} {users[comment?.author_id]?.last_name}</p>
                             <p>{comment?.content}</p>
                         </div>
                     </div>
                 ))}
                 <div className="comment-input">
-                    <div className="person-circle-icon">
-                        <p>{getInitials(cur_user?.id)}</p>
-                    </div>
-                    <div>
+                    <div className="comment-input-container">
+                        <div className="person-circle-icon">
+                            <p>{getInitials(cur_user?.id)}</p>
+                        </div>
                         <textarea
                             name="comment"
                             value={comment}
                             placeholder="Ask a question or post a comment"
+                            onChange={(e) => setComment(e.target.value)}
                         />
-                        <button>Comment</button>
                     </div>
+                    <button>Comment</button>
                 </div>
             </div>
         </div>
