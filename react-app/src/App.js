f5748ebbdc9b6b61099e9b9fa78fdafe8f9ca648
ethanchen7 <ethan.chen7@gmail.com>
2022-06-01 15:57:07 -0700

    single project page adjustments

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -22,7 +22,7 @@ import { authenticate } from "./store/session";
 import { setProjects } from "./store/projects";
 import { setTasks } from "./store/tasks";
 import { setAllUsers, setTeammates } from "./store/teammates";
-import { setComments } from "./store/comments"
+import { setComments } from "./store/comments";
 
 function App() {
   const [loaded, setLoaded] = useState(false);
@@ -67,7 +67,7 @@ function App() {
   useEffect(() => {
     (async () => {
       if (session) {
-        const res = await fetch('/api/comments');
+        const res = await fetch("/api/comments");
         if (res.ok) {
           const data = await res.json();
           dispatch(setComments(data));
@@ -99,13 +99,6 @@ function App() {
         <ProtectedRoute path="/" exact={true}>
           <HomePage />
         </ProtectedRoute>
-        {/* testing */}
-        <Route path="/create-project" exact={true}>
-          <CreateProjectModal />
-        </Route>
-        {/* <Route path="/create-task" exact={true}>
-          <CreateTaskModal />
-        </Route> */}
         <Route path="/my-tasks" exact={true}>
           <MyTasks />
         </Route>
--- react-app/src/components/TeamPreviewContainer/index.js
+++ react-app/src/components/TeamPreviewContainer/index.js
@@ -1,7 +1,7 @@
 import TeamPreview from "../TeamPreview";
 import "./index.css";
 
-const TeamPreviewContainer = ({allUsers, teammates}) => {
+const TeamPreviewContainer = ({ allUsers, teammates }) => {
   return (
     <div className="teammate-container">
       <div className="container-header">
@@ -9,9 +9,16 @@ const TeamPreviewContainer = ({allUsers, teammates}) => {
         <p>Frequent Collaborators</p>
       </div>
       <div className="teammate-container-body">
-        {teammates ? teammates.slice(0, 6).map((userId, idx) => {
-          return <TeamPreview member={allUsers[parseInt(userId)]} key={`member-${idx}`}/>
-        }) : ""}
+        {teammates
+          ? teammates.slice(0, 6).map((userId, idx) => {
+              return (
+                <TeamPreview
+                  member={allUsers[parseInt(userId)]}
+                  key={`member-${idx}`}
+                />
+              );
+            })
+          : ""}
       </div>
     </div>
   );
