d32553d76afd2ae171008223fed1ade92075352c
Lana_K <lana1990.komar@gmail.com>
2022-06-02 15:11:23 -0700

    changes to error messages

--- app/forms/project_form.py
+++ app/forms/project_form.py
@@ -1,30 +1,30 @@
 from xml.dom import ValidationErr
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, DateField, BooleanField, SelectField, SubmitField
-from wtforms.validators import DataRequired, Length, ValidationError
+from wtforms.validators import DataRequired, Length, ValidationError, InputRequired
 from datetime import date
 
 class ProjectForm(FlaskForm):
     name = StringField("name", validators=[DataRequired(),\
-                                           Length(max=255, message="The name should be less than 255 characters")])
+                                           Length(max=255, message="The name should be less than 255 characters.")])
     description = TextAreaField("description", validators=[DataRequired(),\
-                                                           Length(max=2000, message="The description should be less than 2000 characters")])
-    start_date = DateField("start_date", validators=[DataRequired()])
-    end_date = DateField("end_date", validators=[DataRequired()])
+                                                           Length(max=2000, message="The description should be less than 2000 characters.")])
+    start_date = DateField("start_date", validators=[DataRequired(message="This field is required.")])
+    end_date = DateField("end_date", validators=[DataRequired(message="This field is required.")])
     is_private = BooleanField("is_private")
-    priority_id = SelectField("priority", choices=[1, 2, 3])
-    status_id = SelectField("status", choices=[1, 2, 3])
-    members = StringField("members", validators=[DataRequired()])
+    priority_id = SelectField("priority", choices=[1, 2, 3], validators=[InputRequired(message="Please select a field.")])
+    status_id = SelectField("status", choices=[1, 2, 3], validators=[InputRequired(message="Please select a field.")])
+    members = StringField("members", validators=[DataRequired(message="This field is required.")])
 
     def validate_start_date(form, field):
             if (field.data < date.today()):
-                raise ValidationError('Start date cannot be in the past')
+                raise ValidationError('Start date cannot be in the past.')
 
 
     def validate_end_date(form, field):
             if (field.data < date.today()):
-                raise ValidationError('End date cannot be in the past')
+                raise ValidationError('End date cannot be in the past.')
 
     def validate_end_date(form, field):
             if (field.data < form.start_date.data):
-                raise ValidationError('End date must come after start date')
+                raise ValidationError('End date must come after start date.')
