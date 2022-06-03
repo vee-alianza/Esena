1edc8ee3749c8ec492018c7a37475453b283f582
vee-alianza <vee.alianza@gmail.com>
2022-06-03 17:44:41 -0600

    update About

--- react-app/src/components/About/index.js
+++ react-app/src/components/About/index.js
@@ -10,15 +10,23 @@ const About = () => {
     return (
         <>
             <div className="developers-container">
-                <h1>For devs, by devs</h1>
-                <p>
-                    Esena is a project management app where users can create, view, update and delete a project and tasks
-                </p>
+                <div className="dev-upper-section">
+                    <h1>Thank you for visiting Esena!</h1>
+                    <p className="dev-upper-intro">
+                        Esena is a project management app where users can create, view, update and delete a project and tasks
+                    </p>
+                </div>
+                <div className="dev-intro">
+                    <h2>Our amazing team</h2>
+                    <p >
+                        We're a group of talented and creative indiviuals
+                    </p>
+                </div>
                 <div className="inner-dev-container">
                     <div className="dev-profile">
                         <img className="dev-img" src={ethan} />
                         <h3 className="dev-name">Ethan Chen</h3>
-                        {/* <p className="dev-area">Los Angeles, California</p> */}
+                        <p className="dev-title">Software Engineer</p>
                         <div className="logo-container">
                             <a href="https://github.com/ethanchen7"><i class="fa-brands fa-github-square fa-2x" ></i></a>
                             <a href="https://www.linkedin.com/in/ethan-chen-3b7070127/"><i class="fa-brands fa-linkedin fa-2x" ></i></a>
@@ -27,7 +35,7 @@ const About = () => {
                     <div className="dev-profile">
                         <img className="dev-img" src={lana} />
                         <h3 className="dev-name">Lana Komar</h3>
-                        {/* <p className="dev-area">East Bay, California</p> */}
+                        <p className="dev-title">Software Engineer</p>
                         <div className="logo-container">
                             <a href="https://github.com/lanakomar"><i class="fa-brands fa-github-square fa-2x" ></i></a>
                             <a href="https://www.linkedin.com/in/lana-komar/"><i class="fa-brands fa-linkedin fa-2x" ></i></a>
@@ -36,16 +44,19 @@ const About = () => {
                     <div className="dev-profile">
                         <img className="dev-img" src={vee} />
                         <h3 className="dev-name">Vee Alianza</h3>
-                        {/* <p className="dev-area">Eagle Mountain, Utah</p> */}
+                        <p className="dev-title">Software Engineer</p>
                         <div className="logo-container">
                             <a href="https://github.com/vee-alianza"><i class="fa-brands fa-github-square fa-2x" ></i></a>
                             <a href="https://www.linkedin.com/in/vee-alianza/"><i class="fa-brands fa-linkedin fa-2x" ></i></a>
                         </div>
+                        <p className="dev-summary">
+                            As web developer in the making, I am passionate about learning new skills and . I am excited to make full use of these skills in a larger working environment.
+                        </p>
                     </div>
                     <div className="dev-profile">
                         <img className="dev-img" src={xiaowen} />
                         <h3 className="dev-name">Xiaowen Nie</h3>
-                        {/* <p className="dev-area">Sunnyvale, California</p> */}
+                        <p className="dev-title">Software Engineer</p>
                         <div className="logo-container">
                             <a href="https://github.com/xwnnie"><i class="fa-brands fa-github-square fa-2x" ></i></a>
                             <a href="https://www.linkedin.com/in/xiaowennie/"><i class="fa-brands fa-linkedin fa-2x" ></i></a>
@@ -53,6 +64,7 @@ const About = () => {
                     </div>
                 </div>
             </div>
+
         </>
     )
 }
