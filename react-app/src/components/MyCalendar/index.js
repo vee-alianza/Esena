9cc4c0fac62c5825f2705908219cb950ad1040d6
xwnnie <niexiaowen90@gmail.com>
2022-06-03 13:42:47 -0700

    move calendar notes to the top corner

--- react-app/src/components/MyCalendar/index.js
+++ react-app/src/components/MyCalendar/index.js
@@ -4,29 +4,29 @@ import Calendar from "react-awesome-calendar";
 import SideBar from "../SideBar";
 import "./index.css";
 
-const events = [
-  {
-    id: 1,
-    color: "#fd3153",
-    from: "2022-05-02T18:00:00+00:00",
-    to: "2022-05-05T19:00:00+00:00",
-    title: "This is an event",
-  },
-  {
-    id: 2,
-    color: "#1ccb9e",
-    from: "2022-05-01T13:00:00+00:00",
-    to: "2022-05-05T14:00:00+00:00",
-    title: "This is another event",
-  },
-  {
-    id: 3,
-    color: "#3694DF",
-    from: "2022-05-05T13:00:00+00:00",
-    to: "2022-05-05T20:00:00+00:00",
-    title: "This is also another event",
-  },
-];
+// const events = [
+//   {
+//     id: 1,
+//     color: "#fd3153",
+//     from: "2022-05-02T18:00:00+00:00",
+//     to: "2022-05-05T19:00:00+00:00",
+//     title: "This is an event",
+//   },
+//   {
+//     id: 2,
+//     color: "#1ccb9e",
+//     from: "2022-05-01T13:00:00+00:00",
+//     to: "2022-05-05T14:00:00+00:00",
+//     title: "This is another event",
+//   },
+//   {
+//     id: 3,
+//     color: "#3694DF",
+//     from: "2022-05-05T13:00:00+00:00",
+//     to: "2022-05-05T20:00:00+00:00",
+//     title: "This is also another event",
+//   },
+// ];
 const MyCalendar = () => {
     const sessionUser = useSelector((state) => state.session.user);
     const tasksObj = useSelector((state) => state.tasks);
