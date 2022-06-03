56a0e0d4f97fc4234f296fe0f5f6649e360d832c
vee-alianza <vee.alianza@gmail.com>
2022-06-02 23:16:44 -0600

    add dev images

--- react-app/src/components/About/footer.js
+++ react-app/src/components/About/footer.js
@@ -5,38 +5,40 @@ const Footer = () => {
     return (
         <>
             <div className="footer-container">
-                {/* <a href="https://github.com/ethanchen7">Ethan Chen</a>
-
-            <a href="https://github.com/lanakomar">Lana Komar</a>
-
-            <a href="https://github.com/vee-alianza">Vee Alianza</a>
-
-            <a href="https://github.com/xwnnie">Xiaowen Nie</a> */}
-                <div>
-                    <h3>Ethen Chen</h3>
-                    <a href="https://travelr-ec.herokuapp.com/">Travelr</a>
-                    <a href="https://large-medium.herokuapp.com/">Large</a>
-
+                <div className="dev-info-container">
+                    <img className="logo-footer" src="/images/logo.png" />
+                    <div className="dev-info-footer">
+                        <h3>Ethen Chen</h3>
+                        <a href="https://travelr-ec.herokuapp.com/">Travelr</a>
+                        <a href="https://large-medium.herokuapp.com/">Large</a>
+                    </div>
+                    <div className="dev-info-footer">
+                        <h3>Lana Komar</h3>
+                        <a href="https://eventwild.herokuapp.com/events">Event Wild</a>
+                        <a href="https://good-hikes.herokuapp.com/">Good Hikes</a>
+                    </div>
+                    <div className="dev-info-footer">
+                        <h3>Vee Alianza</h3>
+                        <a href="https://mochi-noms.herokuapp.com/">Mochi</a>
+                        <a href="https://duckit-list.herokuapp.com/">Duckit List</a>
+                    </div>
+                    <div className="dev-info-footer">
+                        <h3>Xiaowen Nie</h3>
+                        <a href="https://nature-clikr.herokuapp.com/">NatureClikr</a>
+                        <a href="https://oraql.herokuapp.com/">Oraql</a>
+                    </div>
+                    <div>
+                        <h3>Resources</h3>
+                        <p>Python</p>
+                        <p>Flask</p>
+                        <p>React JS</p>
+                        <p>Redux</p>
+                        <p>Alembic</p>
+                        <p>SQLAlchemy</p>
+                        <p>Socket io</p>
+                        <p></p>
+                    </div>
                 </div>
-                <div>
-                    <h3>Lana Komar</h3>
-                    <a href="https://eventwild.herokuapp.com/events">Event Wild</a>
-                    <a href="https://good-hikes.herokuapp.com/">Good Hikes</a>
-
-                </div>
-                <div>
-                    <h3>Vee Alianza</h3>
-                    <a href="https://mochi-noms.herokuapp.com/">Mochi</a>
-                    <a href="https://duckit-list.herokuapp.com/">Duckit List</a>
-
-                </div>
-                <div>
-                    <h3>Xiaowen Nie</h3>
-                    <a href="https://nature-clikr.herokuapp.com/">NatureClikr</a>
-                    <a href="https://oraql.herokuapp.com/">Oraql</a>
-
-                </div>
-
             </div>
         </>
     )
--- react-app/src/components/About/index.js
+++ react-app/src/components/About/index.js
@@ -10,28 +10,40 @@ const About = () => {
                 </p>
                 <div className="inner-dev-container">
                     <div className="dev-profile">
-                        <img className="dev-img" src="https://user-images.githubusercontent.com/92604480/171740072-78640cb3-f7e2-4539-bc85-7a813440d9be.png"></img>
+                        <img className="dev-img" src="/images/ethan.png" />
                         <h3 className="dev-name">Ethan Chen</h3>
-                        <h4 className="dev-area">California</h4>
-                        <a href="https://github.com/ethanchen7">GitHub</a>
+                        <p className="dev-area">Los Angeles, California</p>
+                        <div className="logo-container">
+                            <a href="https://github.com/ethanchen7"><i class="fa-brands fa-github-square fa-2x" ></i></a>
+                            <a href="https://www.linkedin.com/in/ethan-chen-3b7070127/"><i class="fa-brands fa-linkedin fa-2x" ></i></a>
+                        </div>
                     </div>
                     <div className="dev-profile">
-                        <img className="dev-img" src="https://user-images.githubusercontent.com/92604480/171740072-78640cb3-f7e2-4539-bc85-7a813440d9be.png"></img>
+                        <img className="dev-img" src="https://user-images.githubusercontent.com/92604480/171740072-78640cb3-f7e2-4539-bc85-7a813440d9be.png" />
                         <h3 className="dev-name">Lana Komar</h3>
-                        <h4 className="dev-area">California</h4>
-                        <a href="https://github.com/lanakomar">GitHub</a>
+                        <p className="dev-area">East Bay, California</p>
+                        <div className="logo-container">
+                            <a href="https://github.com/lanakomar"><i class="fa-brands fa-github-square fa-2x" ></i></a>
+                            <a href="https://www.linkedin.com/in/lana-komar/"><i class="fa-brands fa-linkedin fa-2x" ></i></a>
+                        </div>
                     </div>
                     <div className="dev-profile">
-                        <img className="dev-img" src="https://user-images.githubusercontent.com/92604480/171740072-78640cb3-f7e2-4539-bc85-7a813440d9be.png"></img>
+                        <img className="dev-img" src="/images/vee.png" />
                         <h3 className="dev-name">Vee Alianza</h3>
-                        <h4 className="dev-area">Utah</h4>
-                        <a href="https://github.com/vee-alianza">GitHub</a>
+                        <p className="dev-area">Eagle Mountain, Utah</p>
+                        <div className="logo-container">
+                            <a href="https://github.com/vee-alianza"><i class="fa-brands fa-github-square fa-2x" ></i></a>
+                            <a href="https://www.linkedin.com/in/vee-alianza/"><i class="fa-brands fa-linkedin fa-2x" ></i></a>
+                        </div>
                     </div>
                     <div className="dev-profile">
-                        <img className="dev-img" src="https://user-images.githubusercontent.com/92604480/171740072-78640cb3-f7e2-4539-bc85-7a813440d9be.png"></img>
+                        <img className="dev-img" src="/images/xiaowen.png" />
                         <h3 className="dev-name">Xiaowen Nie</h3>
-                        <h4 className="dev-area">California</h4>
-                        <a href="https://github.com/xwnnie">GitHub</a>
+                        <p className="dev-area">Sunnyvale, California</p>
+                        <div className="logo-container">
+                            <a href="https://github.com/xwnnie"><i class="fa-brands fa-github-square fa-2x" ></i></a>
+                            <a href="https://www.linkedin.com/in/xiaowennie/"><i class="fa-brands fa-linkedin fa-2x" ></i></a>
+                        </div>
                     </div>
                 </div>
 
