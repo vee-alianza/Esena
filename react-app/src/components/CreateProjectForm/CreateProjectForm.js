e2d695ab4936d8d8bf3d7bf1e2cb91889949db05
Lana_K <lana1990.komar@gmail.com>
2022-06-02 17:11:42 -0700

    deleted unused variables from the code

--- react-app/src/components/CreateProjectForm/CreateProjectForm.js
+++ react-app/src/components/CreateProjectForm/CreateProjectForm.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Select from "react-select";
 
--- react-app/src/components/EditProjectForm/EditProjectForm.js
+++ react-app/src/components/EditProjectForm/EditProjectForm.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import Select from "react-select";
