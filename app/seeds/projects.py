9d671705d9d811dc386c422ba20972f0f87899b5
ethanchen7 <ethan.chen7@gmail.com>
2022-06-04 13:20:47 -0700

    seeds

--- app/seeds/projects.py
+++ app/seeds/projects.py
@@ -4,18 +4,17 @@ from datetime import datetime
 
 
 def seed_projects():
-    db_project = Project(name="Database Design", description="Design and build database with data mapping and relationships. Migrate and seed database with appropriate data for testing purposes.", start_date=datetime(2022,10,1), end_date=datetime(2022,11,1), is_archived=False, is_private=True, priority_id=1, status_id=1, owner_id=1)
+    esena_email_project = Project(name="Email alerts on Esena", description="When users are away from Esena app, they will receive notifications by email.", start_date=datetime(2022,6,1), end_date=datetime(2023,1,1), is_archived=False, is_private=True, priority_id=1, status_id=1, owner_id=1)
     
-    redux_project = Project(name="Redux Store", description="Be able to pull data from database at the root level, to store and access data at any component of our application, without making additional fetch requests.", start_date=datetime(2022,6,3), end_date=datetime(2022,7,11), is_archived=False, is_private=False, priority_id=1, status_id=1, owner_id=2)
+    nescord_project = Project(name="iOS messager for Nescord", description="An instant messaging app for Nescord.", start_date=datetime(2022,5,1), end_date=datetime(2023,10,11), is_archived=False, is_private=False, priority_id=1, status_id=1, owner_id=5)
     
-    leetcode_project = Project(name="Study Algorithms", description="Be familiar with all forms of data structures, be able to solve problems efficiently with proper implementation of data structures and algorithms", start_date=datetime(2022,8,15), end_date=datetime(2022,10,1), is_archived=False, is_private=False, priority_id=2, status_id=3, owner_id=1)
+    walk_project = Project(name="Map my walk", description="An app that allows users to track where and how far they have walked each day.", start_date=datetime(2022,7,1), end_date=datetime(2022,12,1), is_archived=False, is_private=False, priority_id=2, status_id=3, owner_id=8)
 
-    aws_project = Project(name="AWS S3", description="Implement image upload feature to AWS S3 storages", start_date=datetime(2022,11,3), end_date=datetime(2022,11,10), is_archived=False, is_private=True, priority_id=2, status_id=2, owner_id=6)
+    cupid_project = Project(name="PetCupid", description="An app that connects pet owners with other pet owners and allows them to chat and exchange advice.", start_date=datetime(2022,8,1), end_date=datetime(2022,9,1), is_archived=False, is_private=True, priority_id=2, status_id=2, owner_id=1)
 
-    performance_project = Project(name="Optimizations", description="Investigate performance dips from last week", start_date=datetime(2022,7,12), end_date=datetime(2022,7,15), is_archived=False, is_private=False, priority_id=3, status_id=3, owner_id=8)
+    ml_project = Project(name="Identify Trends with ML", description="Understand the company's current and future machine learning strategy, conduct secondary research on machine learning applications for similar use cases, and interview company's users to understand how and why they conduct multi-studies", start_date=datetime(2022,7,13), end_date=datetime(2022,12,4), is_archived=False, is_private=False, priority_id=3, status_id=3, owner_id=1)
 
-    ux_project = Project(name="UX Improvement", description="Improve UX functionality and design across app components", start_date=datetime(2022,8,12), end_date=datetime(2022,8,23), is_archived=False, is_private=False, priority_id=1, status_id=3, owner_id=4)
-
+    persona_project = Project(name="Align Teams Across Personas", description="The Company has many forms of personas currently in use across the product and engineering organizations. We need to create formalized personas to align teams, projects, and product strategy", start_date=datetime(2022,7,15), end_date=datetime(2022,9,3), is_archived=False, is_private=False, priority_id=1, status_id=3, owner_id=2)
 
     user1 = User.query.get(1)
     user2 = User.query.get(2)
@@ -26,28 +25,43 @@ def seed_projects():
     user7 = User.query.get(7)
     user8 = User.query.get(8)
     
-    db_project.members.append(user2)
-    db_project.members.append(user3)
+    
+    esena_email_project.members.append(user2)
+    esena_email_project.members.append(user3)
+    esena_email_project.members.append(user4)
 
-    redux_project.members.append(user3)
-    redux_project.members.append(user1)
+    nescord_project.members.append(user1)
+    nescord_project.members.append(user4)
+    nescord_project.members.append(user6)
+    nescord_project.members.append(user7)
+    
 
-    leetcode_project.members.append(user2)
-    leetcode_project.members.append(user3)
+    walk_project.members.append(user1)
+    walk_project.members.append(user2)
+    walk_project.members.append(user5)
 
-    aws_project.members.append(user1)
-    aws_project.members.append(user5)
-    aws_project.members.append(user7)
+    cupid_project.members.append(user3)
+    cupid_project.members.append(user4)
+    cupid_project.members.append(user6)
+    cupid_project.members.append(user8)
 
-    performance_project.members.append(user1)
-    performance_project.members.append(user6)
+    ml_project.members.append(user2)
+    ml_project.members.append(user5)
+    ml_project.members.append(user7)
+    ml_project.members.append(user3)
+    ml_project.members.append(user8)
+    
+    persona_project.members.append(user1)
+    persona_project.members.append(user3)
+    persona_project.members.append(user6)
+    persona_project.members.append(user7)
 
-    ux_project.members.append(user8)
-    ux_project.members.append(user1)
-
-    db.session.add(db_project)
-    db.session.add(redux_project)
-    db.session.add(leetcode_project)
+    db.session.add(esena_email_project)
+    db.session.add(nescord_project.members)
+    db.session.add(walk_project)
+    db.session.add(cupid_project)
+    db.session.add(ml_project)
+    db.session.add(persona_project)
 
     db.session.commit()
 
--- app/seeds/tasks.py
+++ app/seeds/tasks.py
@@ -3,20 +3,58 @@ from datetime import datetime
 
 
 def seed_tasks():
-    db_task1 = Task(name="Write database documents", description="User entities, user stories, application functionalities, and database schema",
-                 end_date=datetime(2022, 10, 2), status_id=3, priority_id=1, assigner_id=1, assignee_id=2, project_id=1, is_completed=False)
-    db_task2 = Task(name="Draw DB Diagram", description="Map out one-to-many relationships, join tables for many-to-many relationships",
-                 end_date=datetime(2022, 10, 10), status_id=3, priority_id=1, assigner_id=2, assignee_id=3, project_id=1, is_completed=False)
+    email_task1 = Task(name="Add first name and last name to subject", description="",
+                 end_date=datetime(2022, 6, 6), status_id=3, priority_id=1, assigner_id=2, assignee_id=1, project_id=1, is_completed=True)
+    email_task2 = Task(name="Redo reminder card", description="",
+                 end_date=datetime(2022, 6, 8), status_id=3, priority_id=1, assigner_id=3, assignee_id=1, project_id=1, is_completed=True)
+    email_task3 = Task(name="Hide Configuration menu + get defaults", description="Defaults for email feature",
+                 end_date=datetime(2022, 6, 10), status_id=2, priority_id=3, assigner_id=4, assignee_id=2, project_id=1, is_completed=False)
+    email_task4 = Task(name="Revise the account setting page", description="Users can choose receiving email notifications or not",
+                 end_date=datetime(2022, 6, 18), status_id=1, priority_id=2, assigner_id=4, assignee_id=1, project_id=1, is_completed=False)
+    email_task5 = Task(name="Make sure SC works for all features", description="",
+                 end_date=datetime(2022, 6, 18), status_id=1, priority_id=1, assigner_id=3, assignee_id=3, project_id=1, is_completed=False)
+    email_task6 = Task(name="Clicking on a reminder card should activate reminder tab", description="",
+                 end_date=datetime(2022, 6, 16), status_id=2, priority_id=1, assigner_id=1, assignee_id=1, project_id=1, is_completed=False)
+    email_task7 = Task(name="CSS issues on notification page", description="",
+                 end_date=datetime(2022, 6, 17), status_id=1, priority_id=2, assigner_id=1, assignee_id=4, project_id=1, is_completed=False)
+    email_task8 = Task(name="Remove priority status from overview page", description="",
+                 end_date=datetime(2022, 6, 16), status_id=3, priority_id=2, assigner_id=1, assignee_id=3, project_id=1, is_completed=False)
+    email_task9 = Task(name="Inter IDS in stream subscription", description="",
+                 end_date=datetime(2022, 6, 20), status_id=3, priority_id=3, assigner_id=2, assignee_id=1, project_id=1, is_completed=False)
     
-    redux_task1 = Task(name="Design redux state", description="Need data organization at each level of state", end_date=datetime(2022, 6, 10), assigner_id=2, status_id=2, priority_id=2, assignee_id=1, project_id=2, is_completed=False)
-    redux_task2 = Task(name="Write store actions and reducers", description="Write redux store actions and reducers, careful not to mutate state", end_date=datetime(2022,6,13), status_id=1, priority_id=1, assigner_id=1, assignee_id=3, project_id=2, is_completed=False)
+    nescord_task1 = Task(name="Enable post rendering logging for STL labels", description="", end_date=datetime(2022, 6, 23), assigner_id=5, status_id=2, priority_id=2, assignee_id=1, project_id=2, is_completed=True)
+    nescord_task2 = Task(name="Enable diagnostics for UIKit", description="", end_date=datetime(2022, 6, 25), assigner_id=1, status_id=1, priority_id=2, assignee_id=4, project_id=2, is_completed=True)
+    nescord_task3 = Task(name="User specified schedule in SLI scheduling", description="", end_date=datetime(2022, 6, 20), assigner_id=1, status_id=3, priority_id=3, assignee_id=5, project_id=2, is_completed=False)
+    nescord_task4 = Task(name="Add option to only show key plots", description="", end_date=datetime(2022, 6, 21), assigner_id=5, status_id=2, priority_id=3, assignee_id=1, project_id=2, is_completed=False)
+    nescord_task5 = Task(name="Reset the SPWT SN state on the downlink when server not responding", description="", end_date=datetime(2022, 6, 27), assigner_id=4, status_id=1, priority_id=1, assignee_id=1, project_id=2, is_completed=False)
+    nescord_task6 = Task(name="Add filed stats for user explicit video pause time in video calls", description="", end_date=datetime(2022, 6, 30), assigner_id=1, status_id=1, priority_id=3, assignee_id=5, project_id=2, is_completed=False)
+    nescord_task7 = Task(name="Detect change of replay list upon group update", description="", end_date=datetime(2022, 7, 1), assigner_id=6, status_id=2, priority_id=2, assignee_id=7, project_id=2, is_completed=False)
+    nescord_task8 = Task(name="Migrate restart based on network condition check", description="", end_date=datetime(2022, 7, 1), assigner_id=4, status_id=1, priority_id=1, assignee_id=5, project_id=2, is_completed=False)
+    nescord_task9 = Task(name="Improve stateful ping usage in client transport", description="", end_date=datetime(2022, 7, 2), assigner_id=7, status_id=3, priority_id=2, assignee_id=1, project_id=2, is_completed=False)
+    nescord_task10 = Task(name="Resets in video call", description="Video image rendering is resetting during video meetings", end_date=datetime(2022, 7, 5), assigner_id=5, status_id=1, priority_id=3, assignee_id=1, project_id=2, is_completed=False)
     
-    leetcode_task1 = Task(name="Study heaps", description="Understand how to implement min and max heaps in Python", end_date=datetime(2022,9,12), status_id=3, priority_id=1, assignee_id=2, assigner_id=3, project_id=3, is_completed=False)
-    leetcode_task2 = Task(name="Finish Leetcode Problem 87", description="Solve Leetcode 87, Scramble String", end_date=datetime(2022,9,18), status_id=2, priority_id=3, assignee_id=1, assigner_id=2, project_id=3, is_completed=False)
-    leetcode_task3= Task(name="Backtracking DFS tutorial", description="Watch Abdul Bari's video on Backtracking and recursive DFS on Youtube", end_date=datetime(2022,9,22), status_id=1, priority_id=3,assigner_id=2, assignee_id=3, project_id=3, is_completed=False)
+    walk_task1 = Task(name="Split store test into multiple targets", description="", end_date=datetime(2022,7,10), status_id=3, priority_id=1, assigner_id=8, assignee_id=2, project_id=3, is_completed=True)
+    walk_task2 = Task(name="Track maximum distance from slide window", description="", end_date=datetime(2022,7,13), status_id=2, priority_id=1, assigner_id=1, assignee_id=5, project_id=3, is_completed=True)
+    walk_task3 = Task(name="Update client wiki page", description="", end_date=datetime(2022,7,12), status_id=1, priority_id=1, assigner_id=2, assignee_id=8, project_id=3, is_completed=False)
+    walk_task4 = Task(name="Improve code structure sheet", description="", end_date=datetime(2022,7,16), status_id=2, priority_id=1, assigner_id=1, assignee_id=1, project_id=3, is_completed=False)
+    walk_task5 = Task(name="Add GSO related info on client", description="", end_date=datetime(2022,7,15), status_id=1, priority_id=2, assigner_id=5, assignee_id=1, project_id=3, is_completed=False)
+    walk_task6 = Task(name="Enable strict nullability for UXG", description="", end_date=datetime(2022,7,18), status_id=3, priority_id=2, assigner_id=2, assignee_id=8, project_id=3, is_completed=False)
+    walk_task7 = Task(name="Verify performance of music player", description="", end_date=datetime(2022,7,19), status_id=2, priority_id=2, assigner_id=5, assignee_id=1, project_id=3, is_completed=False)
+    walk_task8 = Task(name="Bring back help section", description="", end_date=datetime(2022,7,21), status_id=1, priority_id=3, assigner_id=8, assignee_id=1, project_id=3, is_completed=False)
+    walk_task9 = Task(name="Remove translation status", description="", end_date=datetime(2022,7,22), status_id=3, priority_id=3, assigner_id=1, assignee_id=2, project_id=3, is_completed=False)
+    walk_task10 = Task(name="Adjust CSS on profile page", description="", end_date=datetime(2022,7,24), status_id=1, priority_id=1, assigner_id=1, assignee_id=5, project_id=3, is_completed=False)
 
-    aws_task1 = Task(name="Set up AWS Storage Bucket", description="Set up aws account and storage buckets with proper permissions", end_date=datetime(2022,11,7), status_id=2, priority_id=2, assignee_id=5, assigner_id=6, project_id=4, is_completed=False)
-    aws_task2 = Task(name="Build upload functions", description="Write code for backend upload POST routes", end_date=datetime(2022,11,8), status_id=1, priority_id=2, assignee_id=1, assigner_id=6, project_id=4, is_completed=False)
+
+    cupid_task1 = Task(name="Use imgix API to optimize the image", description="", end_date=datetime(2022,8,3), status_id=2, priority_id=2, assigner_id=1, assignee_id=3, project_id=4, is_completed=True)
+    cupid_task2 = Task(name="Add Payfast Payment Gateway", description="", end_date=datetime(2022,8,5), status_id=1, priority_id=2, assigner_id=6, assignee_id=4, project_id=4, is_completed=True)
+    cupid_task3 = Task(name="User Status Circle", description="", end_date=datetime(2022,8,8), status_id=3, priority_id=1, assigner_id=8, assignee_id=6, project_id=4, is_completed=False)
+    cupid_task4 = Task(name="Improve private live chat", description="", end_date=datetime(2022,8,10), status_id=2, priority_id=3, assigner_id=1, assignee_id=8, project_id=4, is_completed=False)
+    cupid_task5 = Task(name="Create an invoice section in the User's account", description="", end_date=datetime(2022,8,9), status_id=1, priority_id=2, assigner_id=3, assignee_id=1, project_id=4, is_completed=False)
+    cupid_task6 = Task(name="Move badages to different sections", description="", end_date=datetime(2022,8,14), status_id=2, priority_id=1, assigner_id=4, assignee_id=1, project_id=4, is_completed=False)
+    cupid_task7 = Task(name="Improve file readability", description="", end_date=datetime(2022,8,18), status_id=2, priority_id=2, assigner_id=6, assignee_id=3, project_id=4, is_completed=False)
+    cupid_task8 = Task(name="Improve way of logging error msg", description="", end_date=datetime(2022,8,23), status_id=3, priority_id=1, assigner_id=1, assignee_id=4, project_id=4, is_completed=False)
+    cupid_task9 = Task(name="Cleanup kernel constant and ban class", description="", end_date=datetime(2022,8,30), status_id=2, priority_id=1, assigner_id=8, assignee_id=1, project_id=4, is_completed=False)
+    cupid_task10 = Task(name="Access Admin Panel Upon Installation ", description="", end_date=datetime(2022,9,2), status_id=1, priority_id=3, assigner_id=1, assignee_id=6, project_id=4, is_completed=False)
 
     performance_task1= Task(name="Pinpoint inefficiences", description="Figure out where performance starts to dip, performance tests to find ineffiencies", end_date=datetime(2022,7,13), status_id=2, priority_id=2, assigner_id=8, assignee_id=1, project_id=5, is_completed=False)
     performance_task2= Task(name="Configure load balancers", description="Configure load balancers to maintain server performances", end_date=datetime(2022,7,14), status_id=1, priority_id=1, assigner_id=8, assignee_id=6, project_id=5, is_completed=False)
@@ -24,15 +62,49 @@ def seed_tasks():
     ux_task1= Task(name="Get customer feedback", description='Perform customer surveys and collect data on user experience', end_date=datetime(2022,8,14), status_id=2,priority_id=2, assigner_id=4, assignee_id=1, project_id=6, is_completed=False)
     ux_task2= Task(name="Meet with UX team", description='Discuss UX improvements and what functionalities can be implemented effectively', end_date=datetime(2022,8,16), status_id=1,priority_id=1, assigner_id=4, assignee_id=8, project_id=6, is_completed=False)
 
-    db.session.add(db_task1)
-    db.session.add(db_task2)
-    db.session.add(redux_task1)
-    db.session.add(redux_task2)
-    db.session.add(leetcode_task1)
-    db.session.add(leetcode_task2)
-    db.session.add(leetcode_task3)
-    db.session.add(aws_task1)
-    db.session.add(aws_task2)
+    db.session.add(email_task1)
+    db.session.add(email_task2)
+    db.session.add(email_task3)
+    db.session.add(email_task4)
+    db.session.add(email_task5)
+    db.session.add(email_task6)
+    db.session.add(email_task7)
+    db.session.add(email_task8)
+    db.session.add(email_task9)
+   
+    db.session.add(nescord_task1)
+    db.session.add(nescord_task2)
+    db.session.add(nescord_task3)
+    db.session.add(nescord_task4)
+    db.session.add(nescord_task5)
+    db.session.add(nescord_task6)
+    db.session.add(nescord_task7)
+    db.session.add(nescord_task8)
+    db.session.add(nescord_task9)
+    db.session.add(nescord_task10)
+    
+    db.session.add(walk_task1)
+    db.session.add(walk_task2)
+    db.session.add(walk_task3)
+    db.session.add(walk_task4)
+    db.session.add(walk_task5)
+    db.session.add(walk_task6)
+    db.session.add(walk_task7)
+    db.session.add(walk_task8)
+    db.session.add(walk_task9)
+    db.session.add(walk_task10)
+
+    db.session.add(cupid_task1)
+    db.session.add(cupid_task2)
+    db.session.add(cupid_task3)
+    db.session.add(cupid_task4)
+    db.session.add(cupid_task5)
+    db.session.add(cupid_task6)
+    db.session.add(cupid_task7)
+    db.session.add(cupid_task8)
+    db.session.add(cupid_task9)
+    db.session.add(cupid_task10)
+    
     db.session.add(performance_task1)
     db.session.add(performance_task2)
     db.session.add(ux_task1)
